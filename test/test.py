6fa469205236b8c0459ed76023e48c058e813699
Xu Ma <38414842@qq.com>
2022-08-05 10:47:54 +0800

    update .gitignore

--- test/test.py
+++ test/test.py
@@ -1,13 +1,16 @@
 import base64
 import json
+import time
+
 import requests
 
-with open("test/image0.png", "rb") as f:
+with open("test/a.png", "rb") as f:
     content = f.read()
 data = {"images": [base64.b64encode(content).decode('utf8')]}
 
 while 1:
+    # print(data)
     response = requests.post("http://127.0.0.1:8866/predict/ocr_system",
                              headers={"Content-type": "application/json"},
                              data=json.dumps(data))
-    print(response.json())
+    print(response.elapsed, response.json())
